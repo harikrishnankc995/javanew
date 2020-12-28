ef7805084502fca8940b345f17323e2afb9df18f
Harikrishnan Kc <harikrishnan.kc@ibm.com>
2020-12-28 18:13:20 +0530

    second commit

--- Add.java
+++ Add.java
@@ -3,15 +3,16 @@ public class Add
 {
    public static void main(String args[])
    {
-      int x, y, z;
+      int x, y, z, d;
 
-      System.out.println("Enter two integers to calculate their sum");
+      System.out.println("Enter two integers to calculate their difference");
       Scanner in = new Scanner(System.in);
      
       x = in.nextInt();
       y = in.nextInt();
       z = x + y;
-     
+      d=  x - y;
       System.out.println("Sum of the integers = " + z);
+System.out.println("Difference of the integers = " + d);
    }
 }
